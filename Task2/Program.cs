703674a84b1b22fbaf412095a3e4d86c5caffb24
v-king777 <v.kolpashchikov@gmail.com>
2021-03-04 00:11:35 +0300

    modified:   Task2/Program.cs

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,13 +1,19 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Task2
 {
     class Program
     {
+        static List<Human> oldList = GetListOfHumans();
+
         static void Main(string[] args)
         {
             NumberRead numberRead = new NumberRead();
-            numberRead.NumberEnteredEvent += ShowNumber;
+            numberRead.NumberEnteredEvent += Sorting;
+            
+            ShowListOfHumans(oldList);
 
             while (true)
             {
@@ -18,18 +24,71 @@ namespace Task2
 
                 catch (FormatException)
                 {
-                    Console.WriteLine("\nВведено некорректное значение!");
+                    Console.WriteLine($"\nВведено некорректное значение!\n");
                 }
             }
         }
 
-        static void ShowNumber(int number)
+        static void Sorting(int number)
         {
             switch (number)
             {
-                case 1: Console.WriteLine("\nВыполнена сортировка А-Я"); break;
-                case 2: Console.WriteLine("\nВыполнена сортировка Я-А"); break;
-                case 3: Environment.Exit(0); break;
+                case 1:
+                    ShowListOfHumans(SortByA_Z(oldList));
+                    Console.WriteLine("\nВыполнена сортировка от А до Я\n");
+                    break;
+
+                case 2:
+                    ShowListOfHumans(SortByZ_A(oldList));
+                    Console.WriteLine("\nВыполнена сортировка от Я до А\n"); 
+                    break;
+
+                case 3:
+                    Environment.Exit(0); 
+                    break;
+            }
+        }
+
+        static List<Human> GetListOfHumans()
+        {
+            List<Human> humans = new List<Human>();
+
+            humans.Add(new Human() { Surname = "Иванов" });
+            humans.Add(new Human() { Surname = "Петров" });
+            humans.Add(new Human() { Surname = "Сидоров" });
+            humans.Add(new Human() { Surname = "Зайцев" });
+            humans.Add(new Human() { Surname = "Волков" });
+
+            return humans;
+        }
+
+        static List<Human> SortByA_Z(List<Human> list)
+        {
+            list.Sort(delegate (Human human1, Human human2)
+            {
+                return human1.Surname.CompareTo(human2.Surname);
+            });
+
+            return list;
+        }
+
+        static List<Human> SortByZ_A(List<Human> list)
+        {
+            list.Sort(delegate (Human human1, Human human2)
+            {
+                return human2.Surname.CompareTo(human1.Surname);
+            });
+
+            return list;
+        }
+
+        static void ShowListOfHumans(List<Human> list)
+        {
+            Console.WriteLine("\nСписок фамилий:\n");
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Surname);
             }
         }
     }
