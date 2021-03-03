f2fedc2158145863e90540f08d96aa1c425a3d08
v-king777 <v.kolpashchikov@gmail.com>
2021-03-03 20:21:38 +0300

    modified:   Task2/Program.cs

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -6,7 +6,31 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            NumberRead numberRead = new NumberRead();
+            numberRead.NumberEnteredEvent += ShowNumber;
+
+            while (true)
+            {
+                try
+                {
+                    numberRead.Read();
+                }
+
+                catch (FormatException)
+                {
+                    Console.WriteLine("\nВведено некорректное значение!");
+                }
+            }
+        }
+
+        static void ShowNumber(int number)
+        {
+            switch (number)
+            {
+                case 1: Console.WriteLine("\nВыполнена сортировка А-Я"); break;
+                case 2: Console.WriteLine("\nВыполнена сортировка Я-А"); break;
+                case 3: Environment.Exit(0); break;
+            }
         }
     }
 }
