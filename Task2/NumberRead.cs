8df342733115d430f110f39163776d3b09e796dd
v-king777 <v.kolpashchikov@gmail.com>
2021-03-03 20:21:26 +0300

    new file:   Task2/NumberRead.cs

---
+++ Task2/NumberRead.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task2
+{
+    class NumberRead
+    {
+        public delegate void NumberEnteredDelegate(int number);
+        public event NumberEnteredDelegate NumberEnteredEvent;
+
+        public void Read()
+        {
+            Console.WriteLine("Введите '1' для сортировки А-Я или '2' для сортировки Я-А, '3' - для выхода");
+
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number != 1 && number != 2 && number != 3)
+            {
+                throw new FormatException();
+            }
+
+            NumberEntered(number);
+        }
+
+        protected virtual void NumberEntered(int number)
+        {
+            NumberEnteredEvent?.Invoke(number);
+        }
+    }
+}
